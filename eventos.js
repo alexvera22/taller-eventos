ff03327e74eb1557ebfac7718a94836046669842
alexvera22 <alex.uru.vb@gmail.com>
2023-08-07 18:06:57 -0300

    manejo del div en eventos.js

---
+++ eventos.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+const saludodiv= document.querySelector(".saludo");
+
+saludodiv.addEventListener("click", function(){
+    alert("Hola! Soy el div");
+});
+});
